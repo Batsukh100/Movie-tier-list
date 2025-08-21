713af4fde8042393f5849852cc2b347a50568203
25HP4550 25HP4550 <25HP4550@G301-CC0188.local>
2025-08-21 14:24:28 +0800

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+const Page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">
+        Welcome to the Movie Tier List
+      </h1>
+      <p className="text-lg">Create and share your favorite movie rankings!</p>
+    </div>
+  );
+};
+
+export default Page;
